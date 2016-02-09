da899f7cf1ed97647a2e5900e818ff1717cc5b3d
Luke McDuff <ljm7b2@mail.umkc.edu>
2016-02-08 20:54:57 -0600

    added lab 2-1 (android part 2)

---
+++ Source/Android_Lab_2-1_Source_Code/app/src/main/java/com/example/ljm7b/lab_2_1_login_location_camera/MapsActivity.java
@@ -0,0 +1,216 @@
+package com.example.ljm7b.lab_2_1_login_location_camera;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    public Geocoder geocoder;
+    ImageView userImage;
+    int TAKE_PHOTO_CODE = 0;
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map2);
+        mapFragment.getMapAsync(this);
+
+        userImage = (ImageView)findViewById(R.id.view_photo);
+        userImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+
+                new AlertDialog.Builder(MapsActivity.this)
+                        .setTitle("Choose Photo or Take Photo")
+                        .setMessage("")
+                        .setPositiveButton("Choose Photo", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+                            }
+                        })
+                        .setNegativeButton("Take Photo", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_menu_camera)
+                        .show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            userImage.setImageBitmap(photo);
+            Log.d("CameraDemo", "Pic saved");
+        }
+        else{
+            try {
+                // When an Image is picked
+                if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                        && null != data) {
+                    // Get the Image from data
+
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    imgDecodableString = cursor.getString(columnIndex);
+                    cursor.close();
+                    userImage.setImageBitmap(BitmapFactory
+                            .decodeFile(imgDecodableString));
+
+                } else {
+                    Toast.makeText(this, "You haven't picked Image",
+                            Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        geocoder = new Geocoder(this);
+        StringBuilder userAddress = new StringBuilder();
+        LocationManager userCurrentLocation = (LocationManager) this
+                .getSystemService(Context.LOCATION_SERVICE);
+        LocationListener userCurrentLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        LatLng userCurrentLocationCorodinates = null;
+        double latitute = 0, longitude = 0;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                .checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //show message or ask permissions from the user.
+            return;
+        }
+        //Getting the current location of the user.
+        userCurrentLocation.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                0, 0, userCurrentLocationListener);
+        latitute = userCurrentLocation
+                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                .getLatitude();
+        longitude = userCurrentLocation
+                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                .getLongitude();
+
+        userCurrentLocationCorodinates = new LatLng(latitute, longitude);
+        //Getting the address of the user based on latitude and longitude.
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitute, longitude, 1);
+            Address address = addresses.get(0);
+            userAddress =  new StringBuilder();
+            String street = address.getAddressLine(0);
+            String city = address.getAddressLine(1);
+
+            TextView cityText = (TextView)findViewById(R.id.city);
+            cityText.setText(city);
+
+            TextView streetText = (TextView)findViewById(R.id.street);
+            streetText.setText(street);
+
+
+
+            for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+                userAddress.append(address.getAddressLine(i)).append("\t");
+            }
+            userAddress.append(address.getCountryName()).append("\t");
+
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        //Setting our image as the marker icon.
+        mMap.addMarker(new MarkerOptions().position(userCurrentLocationCorodinates)
+                .title("Your current address.").snippet(userAddress.toString())
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+        //Setting the zoom level of the map.
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userCurrentLocationCorodinates, 12));
+    }
+
+}
