88f72c0ab1bb03b8fc038bb024f2e67b6f74c35c
Luke McDuff <ljm7b2@mail.umkc.edu>
2016-03-16 21:32:19 -0500

    lab 8 push

---
+++ Source/LAB_8_IONIC/LAB_8/www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('app.controllers', [])
+  
+.controller('clinicInfoCtrl', function($scope, $http) {
+    $http.get('http://ec2-52-91-251-221.compute-1.amazonaws.com:8080/CliniConnect/GetClinicInfo').then(function(response){
+        $scope.clinicAddress = response.data.clinicAddress;
+        $scope.clinicPhone = response.data.clinicPhone;
+        $scope.clinicName = response.data.clinicName;
+        $scope.clinicHours = response.data.clinicHours;
+        $scope.clinicEmail = response.data.clinicEmail;
+        console.log(response.data);
+    
+    }, function(error){
+    //there was an error fetching from the server
+    });
+
+})
+   
+.controller('submitVitalsCtrl', function($scope, $http) {
+    $scope.sd = "tacos";
+    
+    $scope.giveResponse = function(resp){
+        if(resp == "safe"){
+            return "Relax, your levels look great!"
+        }
+        else{
+            return "Your levels seem to be out of balance, please contact your Physician ASAP!"
+        }
+    }  
+    var v = this;
+    v.submitVitals = function(){
+        console.log(v.BP);
+        console.log(v.BS);
+    var link = 'http://ec2-52-91-251-221.compute-1.amazonaws.com:8080/CliniConnect/LevelMonitor?' + "BP=" + v.BP + "&" + "BS=" + v.BS;
+        
+    $http.get(link).then(function (response){
+        $scope.status = $scope.giveResponse(response.data.status);
+        });
+    }
+})
+    
